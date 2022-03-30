38b844fe772fb6707f91783007a402fc8c84f112
Bianca Franco <95880578+biancafranco1@users.noreply.github.com>
2022-03-30 16:21:14 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+import Stack from './routes/Stack'
+
+import {NavigationContainer} from '@react-navigation/native'
+
+export default () => (
+  
+  <NavigationContainer>
+
+      <Stack/>
+
+  </NavigationContainer>
+
+)
+
+
+//const Estilo = StyleSheet.create(
+ // {
+ //   container: {
+  //    flexGrow: 1,
+   //   justifyContent: 'center',
+  //    alignItems: 'center'
+ //   }
+ // }
+//)
